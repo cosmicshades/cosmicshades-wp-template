33b04d82d51277d2a922fd7cef34f7e9aacc20a8
John Sylvain <jsylvain007@gmail.com>
2015-07-25 17:53:42 -0500

    init commit

---
+++ Gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefix = require('gulp-autoprefixer');
+
+
+gulp.task('sass', function(){
+	gulp.src('./scss/**/*.scss')
+		.pipe( sass().on('error', sass.logError) )
+		.pipe(autoprefix())
+		.pipe( gulp.dest('./css/'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('./scss/**/*.scss', ['sass']);
+})
+
+gulp.task('default',['watch']);
